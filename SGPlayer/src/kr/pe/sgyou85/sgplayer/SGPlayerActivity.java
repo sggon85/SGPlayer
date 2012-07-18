1687ba9a625dcce05d42496ff9c816f056c75770
sgyou <sggon85@gmail.com>
2012-07-18 13:24:11 +0900

    시작...

---
+++ SGPlayer/src/kr/pe/sgyou85/sgplayer/SGPlayerActivity.java
@@ -0,0 +1,13 @@
+package kr.pe.sgyou85.sgplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SGPlayerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
