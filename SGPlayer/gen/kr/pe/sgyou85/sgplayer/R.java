b1e3c77b0346adccce38d7ea15972a2c405b2082
sggon85 <sggon85@gmail.com>
2012-07-31 06:46:46 +0900

    UI fix

--- SGPlayer/gen/kr/pe/sgyou85/sgplayer/R.java
+++ SGPlayer/gen/kr/pe/sgyou85/sgplayer/R.java
@@ -11,14 +11,34 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int borderline=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int textviewborder=0x7f020002;
     }
     public static final class id {
-        public static final int m_btnPlay=0x7f050001;
-        public static final int m_btnStop=0x7f050002;
+        public static final int linearLayout1=0x7f05000d;
+        public static final int linearLayout2=0x7f050002;
+        public static final int linearLayout3=0x7f050004;
+        public static final int linearLayout4=0x7f050013;
+        public static final int m_btnNext=0x7f050012;
+        public static final int m_btnNextPoint=0x7f050016;
+        public static final int m_btnPause=0x7f050010;
+        public static final int m_btnPlay=0x7f05000f;
+        public static final int m_btnPre=0x7f05000e;
+        public static final int m_btnPrePoint=0x7f050014;
+        public static final int m_btnSavePoint=0x7f050015;
+        public static final int m_btnStop=0x7f050011;
         public static final int m_llContainer=0x7f050000;
-        public static final int m_sbProgress=0x7f050003;
-        public static final int m_tvProgressTime=0x7f050004;
+        public static final int m_llMoveButtons=0x7f050006;
+        public static final int m_minus1=0x7f050009;
+        public static final int m_minus3=0x7f050008;
+        public static final int m_minus5=0x7f050007;
+        public static final int m_plus1=0x7f05000a;
+        public static final int m_plus3=0x7f05000b;
+        public static final int m_plus5=0x7f05000c;
+        public static final int m_sbProgress=0x7f050005;
+        public static final int m_tvFileName=0x7f050001;
+        public static final int m_tvProgressTime=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
